3ba2f79c46491f571c2952c914610e85522a24b2
Slapzy1 <95937486+Slapzy1@users.noreply.github.com>
2021-12-20 15:57:49 -0300

    Oculto.py
    Flag oculta

---
+++ Oculto.py
@@ -0,0 +1,9 @@
+num = aca hay flag 123459
+if num == no hay flag:
+    print("aca no hay flag")
+elif num == cerca de la flag:
+    print("cerca de la flag")
+elif num == aca hay flag 123459:
+    print("123459")
+else:
+    print("aca no hay nada")
